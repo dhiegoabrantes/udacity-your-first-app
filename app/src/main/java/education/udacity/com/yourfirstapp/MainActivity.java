80d90ba02630f11977697f102e60176af9967cdc
Dhiego Abrantes <dhiegoabrantes@gmail.com>
2016-09-13 01:36:56 -0300

    Final commit

---
+++ app/src/main/java/education/udacity/com/yourfirstapp/MainActivity.java
@@ -0,0 +1,23 @@
+package education.udacity.com.yourfirstapp;
+
+import android.content.res.Configuration;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView iv = (ImageView) findViewById(R.id.imageView);
+
+        Glide.with(this).load(R.mipmap.brazil).into(iv);
+    }
+}
